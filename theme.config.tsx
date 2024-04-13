b7375e93b326754158842a2d520529ee6b42d327
Matisse <166837137+EffxctMP@users.noreply.github.com>
2024-04-13 16:37:47 +0200

    upd

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>🍃 ZaZa PlaZa Server Information</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/EffxctMP/zazaplaza/tree/main',
   },
   chat: {
     link: 'https://discord.gg/H6NrzZKupE',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/EffxctMP/zazaplaza/tree/main',
   footer: {
     text: '🍃 ZaZa PlaZa',
   },
