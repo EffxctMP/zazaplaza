508f9d43582dc918274adeb4ca46fc72609f06db
Matisse <effxct.mp@gmail.com>
2024-04-14 05:19:43 +0200

    Card Test 2

--- components/cards.js
+++ components/cards.js
@@ -1,8 +1,35 @@
-import { Card, Cards } from 'fumadocs-ui/components/card';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-<MDX
-  components={{
-    Card: (props) => <Card {...props} />,
-    Cards: (props) => <Cards {...props} />,
-  }}
-/>
+const Cards = ({ children }) => {
+  return (
+    <div className="cards-container">
+      {children}
+    </div>
+  );
+};
+
+Cards.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Cards;
+
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Card = ({ href, title, description }) => {
+  return (
+    <a href={href} className="card">
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </a>
+  );
+};
+
+Card.propTypes = {
+  href: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
