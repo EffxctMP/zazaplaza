ccada2a0043cea59b3c1390faf29c1f6cafecca1
Matisse <effxct.mp@gmail.com>
2024-04-14 06:24:16 +0200

    test x

--- components/cards.js
+++ components/cards.js
@@ -1,35 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
 export const Card = ({ href, title, description }) => {
-  return (
-    <a href={href} className="card">
-      <h2>{title}</h2>
-      <p>{description}</p>
-    </a>
-  );
-};
-
-Card.propTypes = {
-  href: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-};
-
-export const Cards = ({ children }) => {
-  return (
-    <div className="cards-container">
-      {children}
-    </div>
-  );
-};
-
-Cards.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-
-export function Card({ href, title, description }) {
+    return (
+      <a href={href} className="card">
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </a>
+    );
+  };
+  
+  Card.propTypes = {
+    href: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  };
+  
+  export const Cards = ({ children }) => {
+    return (
+      <div className="cards-container">
+        {children}
+      </div>
+    );
+  };
+  
+  Cards.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+  
+  export function StyledCard({ href, title, description }) {
     const cardStyle = {
       backgroundColor: '#f8f9fa',
       border: '1px solid #e9ecef',
@@ -44,4 +40,4 @@ export function Card({ href, title, description }) {
         <p>{description}</p>
       </a>
     );
-  }
+  };
